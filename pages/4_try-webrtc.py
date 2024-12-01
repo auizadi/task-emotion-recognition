fe630ad9438755b05e30a8f4f1b5d3c2f36c107d
auizadi <auizaldi.21026@mhs.unesa.ac.id>
2024-12-01 14:35:37 +0700

    commit

--- pages/4_try-webrtc.py
+++ pages/4_try-webrtc.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
+import av
 
-st.title("Webrtc")
-webrtc_streamer(key="Example")
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+st.title("Streamlit WebRTC - Uji Kamera")
+webrtc_streamer(
+    key="example",
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    rtc_configuration={  
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
